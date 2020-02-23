08e13b12d6fe0464250758c01cb90a26bcb9aaf7
Ruby <carefree0127@hotmail.com>
2020-02-23 09:58:20 +0800

    元件分割Header、Article、Tag、Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { FooterComponent } from './footer/footer.component';
+import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ArticlesComponent,
+    FooterComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
